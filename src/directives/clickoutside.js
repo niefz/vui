f46b6cd6cfe6a145d7f9da8af459fa49e05fb9e0
libbyyounh <libbyyounh@gmail.com>
2018-03-05 21:27:24 +0800

    modify clickoutside

--- src/directives/clickoutside.js
+++ src/directives/clickoutside.js
@@ -1,13 +1,13 @@
 /**
  * Created by niefz on 2018/1/8.
  */
-import Vue from 'vue'
+import Vue from 'vue';
 
-const nodeList = []
-const ctx = '@@clickoutsideContext'
+const nodeList = [];
+const ctx = '@@clickoutsideContext';
 
-let startClick
-let seed = 0
+let startClick;
+let seed = 0;
 
 function createDocumentHandler (el, binding, vnode) {
   return function (mouseup = {}, mousedown = {}) {
@@ -20,14 +20,14 @@ function createDocumentHandler (el, binding, vnode) {
       el === mouseup.target ||
       (vnode.context.popperElm &&
       (vnode.context.popperElm.contains(mouseup.target) ||
-      vnode.context.popperElm.contains(mousedown.target)))) return
+      vnode.context.popperElm.contains(mousedown.target)))) return;
 
     if (binding.expression &&
       el[ctx].methodName &&
       vnode.context[el[ctx].methodName]) {
-      vnode.context[el[ctx].methodName]()
+      vnode.context[el[ctx].methodName]();
     } else {
-      el[ctx].bindingFn && el[ctx].bindingFn()
+      el[ctx].bindingFn && el[ctx].bindingFn();
     }
   }
 }
@@ -42,8 +42,8 @@ function createDocumentHandler (el, binding, vnode) {
  */
 export default {
   bind (el, binding, vnode) {
-    nodeList.push(el)
-    const id = seed++
+    nodeList.push(el);
+    const id = seed++;
     el[ctx] = {
       id,
       documentHandler: createDocumentHandler(el, binding, vnode),
@@ -53,20 +53,20 @@ export default {
   },
 
   update (el, binding, vnode) {
-    el[ctx].documentHandler = createDocumentHandler(el, binding, vnode)
-    el[ctx].methodName = binding.expression
-    el[ctx].bindingFn = binding.value
+    el[ctx].documentHandler = createDocumentHandler(el, binding, vnode);
+    el[ctx].methodName = binding.expression;
+    el[ctx].bindingFn = binding.value;
   },
 
   unbind (el) {
-    let len = nodeList.length
+    let len = nodeList.length;
 
     for (let i = 0; i < len; i++) {
       if (nodeList[i][ctx].id === el[ctx].id) {
-        nodeList.splice(i, 1)
-        break
+        nodeList.splice(i, 1);
+        break;
       }
     }
-    delete el[ctx]
+    delete el[ctx];
   }
 }
