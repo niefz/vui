1fe81feaa3bc6061396f5f3809617ddc4d237b05
niefz <fengzhao1.nie@midea.com>
2018-04-16 10:25:40 +0800

    update npm

--- src/index.js
+++ src/index.js
@@ -3,52 +3,64 @@
  */
 
 // import Affix from './components/affix/index.js';
-// import Alert from './components/alert/index.js';
-// import Anchor from './components/anchor/index.js';
-// import Avatar from './components/avatar/index.js';
-// import Backtop from './components/backtop/index.js';
-// import Badge from './components/badge/index.js';
-// import Breadcrumb from './components/breadcrumb/index.js';
+import Alert from './components/alert/index.js';
+import Anchor from './components/anchor/index.js';
+import AnchorLink from './components/anchor-link/index.js';
+import Avatar from './components/avatar/index.js';
+import Backtop from './components/backtop/index.js';
+import Badge from './components/badge/index.js';
+import Breadcrumb from './components/breadcrumb/index.js';
+import BreadcrumbItem from './components/breadcrumb-item/index.js';
 import Button from './components/button/index.js';
 import ButtonGroup from './components/button-group/index.js';
 // import Calendar from './components/calendar/index.js';
 // import Card from './components/card/index.js';
 // import Carousel from './components/carousel/index.js';
 // import Cascader from './components/cascader/index.js';
-// import Checkbox from './components/checkbox/index.js';
-// import Col from './components/col/index.js';
+import Checkbox from './components/checkbox/index.js';
+import CheckboxGroup from './components/checkbox-group/index.js';
+import Col from './components/col/index.js';
 // import Collapse from './components/collapse/index.js';
 // import DateTimePicker from './components/datetimepicker/index.js';
-// import Divider from './components/divider/index.js';
-// import DropDown from './components/dropdown/index.js';
-// import DropdownMenu from './components/dropdown-menu/index.js';
-// import DropdownMenuItem from './components/dropdown-menu-item/index.js';
+import Divider from './components/divider/index.js';
+import DropDown from './components/dropdown/index.js';
+import DropdownMenu from './components/dropdown-menu/index.js';
+import DropdownMenuItem from './components/dropdown-menu-item/index.js';
 // import Empty from './components/empty/index.js';
 // import Exception from './components/exception/index.js';
 // import Form from './components/form/index.js';
+// import FormItem from './components/form-item/index.js';
 import Icon from './components/icon/index.js';
-// import Input from './components/input/index.js';
+import Input from './components/input/index.js';
+// import InputNumber from './components/input-number/index.js';
 // import Layout from './components/layout/index.js';
 // import LoadingBar from './components/loadingbar/index.js';
 // import Menu from './components/menu/index.js';
-// import Message from './components/message/index.js';
-// import Modal from './components/modal/index.js';
-// import Notification from './components/notification/index.js';
+// import MenuItem from './components/menu-item/index.js';
+// import MenuItemGroup from './components/menu-item-group/index.js';
+import Message from './components/message/index.js';
+import Modal from './components/modal/index.js';
+import Notification from './components/notification/index.js';
 // import Pagination from './components/pagination/index.js';
 // import Popover from './components/popover/index.js';
-// import Progress from './components/progress/index.js';
-// import Radio from './components/radio/index.js';
+import Progress from './components/progress/index.js';
+import Radio from './components/radio/index.js';
+import RadioGroup from './components/radio-group/index.js';
 // import Rate from './components/rate/index.js';
-// import Row from './components/row/index.js';
+import Row from './components/row/index.js';
 // import ScrollBar from './components/scrollbar/index.js';
-// import Select from './components/select/index.js';
+import Select from './components/select/index.js';
+import SelectMenu from './components/select-menu/index.js';
+import SelectMenuOption from './components/select-menu-option/index.js';
 // import Slider from './components/slider/index.js';
 // import Spin from './components/spin/index.js';
 // import Steps from './components/steps/index.js';
 // import Switch from './components/switch/index.js';
 // import Table from './components/table/index.js';
 // import Tabs from './components/tabs/index.js';
-// import Tag from './components/tag/index.js';
+import Tag from './components/tag/index.js';
+import TagGroup from './components/tag-group/index.js';
+import Textarea from './components/textarea/index.js';
 // import TimeLine from './components/timeline/index.js';
 // import Tooltip from './components/tooltip/index.js';
 // import Transfer from './components/transfer/index.js';
@@ -58,49 +70,62 @@ import Icon from './components/icon/index.js';
 
 const components = [
   // Affix,
-  // Alert,
-  // Anchor,
-  // Avatar,
-  // Backtop,
-  // Badge,
-  // Breadcrumb,
+  Alert,
+  Anchor,
+  AnchorLink,
+  Avatar,
+  Backtop,
+  Badge,
+  Breadcrumb,
+  BreadcrumbItem,
   Button,
   ButtonGroup,
   // Calendar,
   // Card,
   // Carousel,
   // Cascader,
-  // Checkbox,
-  // Col,
+  Checkbox,
+  CheckboxGroup,
+  Col,
   // Collapse,
   // DateTimePicker,
-  // Divider,
-  // DropDown,
-  // DropdownMenu,
-  // DropdownMenuItem,
+  Divider,
+  DropDown,
+  DropdownMenu,
+  DropdownMenuItem,
   // Empty,
   // Exception,
   // Form,
+  // FormItem,
   Icon,
-  // Input,
+  Input,
+  // InputNumber,
   // Layout,
   // LoadingBar,
   // Menu,
-  // Modal,
+  // MenuItem,
+  // MenuItemGroup,
+  Modal,
   // Pagination,
   // Popover,
-  // Progress,
-  // Radio,
+  Progress,
+  Radio,
+  RadioGroup,
   // Rate,
-  // Row,
+  Row,
   // ScrollBar,
-  // Select,
+  Select,
+  SelectMenu,
+  SelectMenuOption,
   // Slider,
+  // Spin,
   // Steps,
   // Switch,
   // Table,
   // Tabs,
-  // Tag,
+  Tag,
+  TagGroup,
+  Textarea,
   // TimeLine,
   // Tooltip,
   // Transfer,
@@ -122,8 +147,8 @@ const install = (Vue, opts = {}) => {
   VUI.size = opts.size || '';
 
   // Vue.prototype.$spin = Spin.service;
-  // Vue.prototype.$notify = Notification;
-  // Vue.prototype.$message = Message;
+  Vue.prototype.$notify = Notification;
+  Vue.prototype.$message = Message;
 
   Vue.prototype.$VUI = VUI;
 };
@@ -138,49 +163,62 @@ const exports = {
   // i18n: locale.i18n,
   install,
   // Affix,
-  // Alert,
-  // Anchor,
-  // Avatar,
-  // Backtop,
-  // Badge,
-  // Breadcrumb,
+  Alert,
+  Anchor,
+  AnchorLink,
+  Avatar,
+  Backtop,
+  Badge,
+  Breadcrumb,
+  BreadcrumbItem,
   Button,
   ButtonGroup,
   // Calendar,
   // Card,
   // Carousel,
   // Cascader,
-  // Checkbox,
-  // Col,
+  Checkbox,
+  CheckboxGroup,
+  Col,
   // Collapse,
   // DateTimePicker,
-  // Divider,
-  // DropDown,
-  // DropdownMenu,
-  // DropdownMenuItem,
+  Divider,
+  DropDown,
+  DropdownMenu,
+  DropdownMenuItem,
   // Empty,
   // Exception,
   // Form,
+  // FormItem,
   Icon,
-  // Input,
+  Input,
+  // InputNumber,
   // Layout,
   // LoadingBar,
   // Menu,
-  // Modal,
+  // MenuItem,
+  // MenuItemGroup,
+  Modal,
   // Pagination,
   // Popover,
-  // Progress,
-  // Radio,
+  Progress,
+  Radio,
+  RadioGroup,
   // Rate,
-  // Row,
+  Row,
   // ScrollBar,
-  // Select,
+  Select,
+  SelectMenu,
+  SelectMenuOption,
   // Slider,
+  // Spin,
   // Steps,
   // Switch,
   // Table,
   // Tabs,
-  // Tag,
+  Tag,
+  TagGroup,
+  Textarea,
   // TimeLine,
   // Tooltip,
   // Transfer,
