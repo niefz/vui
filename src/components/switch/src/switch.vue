8b4feaf753ad379488f6cc79853b196aa230fc29
libbyyounh <libbyyounh@gmail.com>
2018-04-18 22:52:51 +0800

    edit switch component

---
+++ src/components/switch/src/switch.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-switch"></div>
+</template>
+<script>
+  export default {
+    name: 'Switch',
+    componentName: 'Switch',
+    props: {
+    },
+    computed: {
+    },
+    methods: {
+    },
+  };
+</script>
