d342a8c3de06c71eec45efd8370ce5b16b1e0c76
libbyyounh <libbyyounh@gmail.com>
2018-04-25 23:42:31 +0800

    edit rate component

---
+++ src/components/rate/src/rate.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="v-rate">
+    <span
+      v-for="item in points"
+      class="v-rate__item"
+    >
+      <slot></slot>
+    </span>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Rate',
+    componentName: 'Rate',
+    props: {
+      points: {
+        type: Number,
+        default: 5,
+      },
+    },
+    data() {
+      return {};
+    },
+    computed: {
+    },
+    watch: {
+    },
+    methods: {
+    },
+    created() {
+    },
+    mounted() {
+    },
+  };
+</script>
