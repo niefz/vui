30fb16cab42b97886e8476d88b6953e77c3d2d16
libbyyounh <libbyyounh@gmail.com>
2018-06-28 21:01:14 +0800

    edit transfer

---
+++ src/components/transfer/src/transfer-panel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div 
+    class="v-transfer--panel" 
+    :class="[
+      {
+      }
+    ]"
+  >
+  </div>
+</template>
+<script>
+  import Icon from '@/components/icon';
+  export default {
+    name: 'TransferPanel',
+    componentName: 'TransferPanel',
+    components: {
+      Icon,
+    },
+    inheritAttrs: false,
+    props: {
+    },
+    data() {
+      return {};
+    },
+    computed: {
+    },
+    watch: {
+    },
+    methods: {
+    },
+    created() {
+    },
+    mounted() {
+    },
+  };
+</script>
